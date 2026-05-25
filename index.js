cc841483a2a66dd923665caf8cb1a6235d3d1253
feranmiogundiran93 <bamisioluwaferanmi@gmail.com>
2026-05-25 11:15:37 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const router = require("./routes/todoRoutes");
+const live_url = "mongodb+srv://bamisioluwaferanmi_db_user:aanuoluwapO93@cluster0.gftucoa.mongodb.net/AuthenticationDB?appName=Cluster0";
+const local_url = "mongodb://localhost:27017/UserDB";
+
+mongoose
+.connect(local_url)
+.then(() => console.log("MongoDB Connected"))
+.catch((err) => console.error("Connection Error: ", err));
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/todos", router);
+
+app.get('/', (req, res) => {res.send('Hello World');});
+const port = 3000;
+app.listen(port, () => {console.log(`Server is running on port ${port}`);});
